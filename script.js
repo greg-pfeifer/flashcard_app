f40f06168a4c9b3c2eadab084b86d45aa3cf8bf9
Greg P <gpfeif20@gmail.com>
2023-03-16 17:57:10 -0400

    all done

--- script.js
+++ script.js
@@ -1,24 +1,78 @@
 
 
-/* FLASH CARD APP - PSEUDOCODE
 
-1. When page loads, show user a start button
+var cardDisplay = document.querySelector('.card-display');
+var startBtn = document.querySelector('#start');
+var questionDisplay = document.querySelector('#question-display');
+var timerDisplay = document.querySelector('#timer-display');
+var answerDisplay = document.querySelector('#answer-display');
+var nextBtn = document.querySelector('#next');
 
-2. When user clicks start button, show the first flash card 
-    2a. Show card question and start a five second timer
-        2aa. Create a variable to track current card index
-        2ab. Create a function that grabs the current card and displays the question to the window.
-    2b. When timer has completed, show the answer
-    2c. Show a next button to allow user to transition to next card
+var currentCardIndex = 0;
+var card;
+var count = 5;
 
-3. When the end of the card array has been reached, show the user a confirmation asking if they would like to restart the flashcards. 
-    3a. If they would like to restart from the beginning
-        3aa. If they confirm yes, reset card index to zero and show first card
-        3ab. If they confirm no, show a goodbye message
+function promptUserToRestart() {
+  var userChoice = confirm('Would you like to restart?');
 
-*/
+  if (userChoice) {
+    displayCard();
+  } else {
+    questionDisplay.innerText = 'Have a great day!';
+    answerDisplay.classList.add('hide');
+    nextBtn.classList.add('hide');
+  }
+}
+function showAnswer() {
+  answerDisplay.innerText = card.answer;
+  timerDisplay.classList.add('hide');
+  nextBtn.classList.remove('hide');
+  answerDisplay.classList.remove('hide');
+
+  currentCardIndex++;
+
+  if (currentCardIndex === cards.length) {
+    currentCardIndex = 0;
+    nextBtn.classList.add('hide');
+    promptUserToRestart();
+  }
+}
+
+function startTimer() {
+  timerDisplay.classList.remove('hide');
+  timerDisplay.innerText = '5';
+  
+  var timer = setInterval(function() {
+    count--;
+    timerDisplay.innerText = count;
+    
+    if (!count) {
+      clearInterval(timer);
+      count = 5;
+      showAnswer();
+    }
+  }, 1000);
+}
 
 
+function displayCard() {
+  
+  card = cards[currentCardIndex];
 
+  cardDisplay.classList.remove('hide');
+  
+  questionDisplay.innerText = card.question;
 
+  answerDisplay.classList.add('hide');
+  nextBtn.classList.add('hide');  
+  startTimer();
+}
+
+function startFlashCards() {
+  startBtn.classList.add('hide');
+  displayCard();
+}
+
+startBtn.addEventListener('click', startFlashCards);
+nextBtn.addEventListener('click', displayCard);
 
