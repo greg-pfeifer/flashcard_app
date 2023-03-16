cd22ea0e686cd912750bd6065dbe62777857224e
Greg P <gpfeif20@gmail.com>
2023-03-16 11:08:39 -0400

    initial commit

---
+++ cards.js
@@ -0,0 +1,17 @@
+
+// First, set up the array of data
+
+var cards = [
+    {
+        question: 'What does the acronym DOM stand for?',
+        answer: 'Document Object Model'
+    },
+    {
+        question: 'Which syntax is used to call a function?',
+        answer: 'function name followed by parentheses - func()'
+    },
+    {
+        question: 'What are some primitive values?',
+        answer: 'Number, String, Boolean, Undefined, Null, Symbol, BigInt'
+    }
+];
